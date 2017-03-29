c97b58096965eff020ae8a86a22b3839c1978051
Shaun Webster <swebster@valcom.com>
2017-03-29 16:21:50 -0400

    Initial commit to make the plugin visible to WordPress

---
+++ acf-pro-layers.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: ACF Pro Layers
+Plugin URI: https://github.com/pressden/acf-pro-layers
+Description: ACF Pro Layers (APL) is a WordPress plugin that extends ACF Pro with pre-defined content modules for complex page layouts.
+Version: 1.0.0
+Author: D.S. Webster
+Author URI: http://pressden.com/
+License: GPLv2 or later
+Text Domain: acf_pro_layers
+*/
