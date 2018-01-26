52277443d009fe3f586ae381293d5a9e163be608
D.S. Webster <dswebs.me@gmail.com>
2018-01-26 17:24:01 -0500

    Add a unique ID to the anchor layer

--- apl-templates/anchor/template.php
+++ apl-templates/anchor/template.php
@@ -8,4 +8,4 @@ $name = $layer['name'];
 $css_classes = ( isset( $layer['css_classes'] ) ) ? $layer['css_classes'] : null;
 ?>
 
-<a name="<?php echo $name; ?>" class="anchor <?php echo $css_classes; ?>"></a>
+<a name="<?php echo $name; ?>" id="<?php echo $apl_unique_id; ?>" class="anchor <?php echo $css_classes; ?>"></a>
