c816c5691cb837a5a5ded3d41da86363d30c9045
D.S. Webster <dswebs.me@gmail.com>
2018-03-02 08:23:28 -0500

    Improve the markup structure and fix minor related content issues

--- apl-templates/related-content/layout/grid.php
+++ apl-templates/related-content/layout/grid.php
@@ -20,9 +20,6 @@ foreach( $related_posts as $related ) {
   // the CSS classes
   $related_css_classes = null;
   
-  // the container
-  $related_post_container = 'div';
-  
   // get initial values from the post object
   if( $related_post ) {
     $related_post_title = $related_post->post_title;
@@ -35,6 +32,8 @@ foreach( $related_posts as $related ) {
     $related_post_title = ( $related['title'] ) ? $related['title'] : $related_post_title;
     $related_post_excerpt = ( $related['excerpt'] ) ? apply_filters( 'the_excerpt', $related['excerpt'] ) : $related_post_excerpt;
     $related_post_image = ( $related['image'] ) ? wp_get_attachment_image( $related['image']['ID'], 'post_thumbnail', false, array( 'class' => 'img-fluid mx-auto d-block' ) ) : $related_post_image;
+		$related_post_button_text = ( $related['button_text'] ) ? $related['button_text'] : $button_text;
+		$related_post_button_classes = ( $related['button_classes'] ) ? $related['button_class'] : $button_classes;
     if( $related['external_url'] ) {
       $related_post_url = $related['external_url'];
       $related_post_external_url = true;
@@ -42,41 +41,48 @@ foreach( $related_posts as $related ) {
     $related_css_classes = $related['css_classes'];
   }
   
-  $related_post_container = ( !$show_buttons && $related_post_url ) ? 'a' : 'div';
   $related_post_href = ( $related_post_url ) ? 'href="' . $related_post_url . '"' : null;
   $related_post_target = ( $related_post_external_url ) ? 'target="_blank"' : null;
+	$related_post_anchor_open = ( $related_post_href ) ? '<a ' . $related_post_href . ' ' . $related_post_target . '>' : '';
+	$related_post_anchor_close = ( $related_post_href ) ? '</a>' : '';
+	$related_post_button_open = ( $related_post_href ) ? '<a ' . $related_post_href . ' class="' . $related_post_button_classes . '" ' . $related_post_target . '>' : '';
+	$related_post_button_close = ( $related_post_href ) ? '</a>' : '';
   
   ?>
   
-  <<?php echo $related_post_container; ?> <?php echo ( $related_post_container == 'a' ) ? $related_post_href . ' ' . $related_post_target : ''; ?> class="related-post <?php echo $related_css_classes; ?> col-sm-<?php echo $column_size; ?>">
+  <div class="related-post <?php echo $related_css_classes; ?> col-sm-<?php echo $column_size; ?>">
     
     <?php if( $show_images && $related_post_image ): ?>
       <div class="related-post-image media-container">
-        <?php echo $related_post_image; ?>
+				<?php echo $related_post_anchor_open; ?>
+				<?php echo $related_post_image; ?>
+				<?php echo $related_post_anchor_close; ?>
       </div>
     <?php endif; ?>
     
     <?php if( $show_titles ): ?>
-      <h3 class="related-post-title text-center">
+      <h3 class="related-post-title">
+				<?php echo $related_post_anchor_open; ?>
         <?php echo $related_post_title; ?>
+				<?php echo $related_post_anchor_close; ?>
       </h3>
     <?php endif; ?>
     
     <?php if( $show_excerpts && $related_post_excerpt ): ?>
-      <div class="related-post-excerpt text-center">
+      <div class="related-post-excerpt">
         <?php echo $related_post_excerpt; ?>
       </div>
     <?php endif; ?>
     
     <?php if( $show_buttons && $related_post_url ): ?>
-      <div class="related-post-button text-center">
-        <a <?php echo $related_post_href; ?> <?php echo $related_post_target; ?> class="btn btn-primary">
-          <?php echo $button_text; ?>
-        </a>
+      <div class="related-post-button">
+				<?php echo $related_post_button_open; ?>
+        <?php echo $related_post_button_text; ?>
+				<?php echo $related_post_button_close; ?>
       </div>
     <?php endif; ?>
     
-  </<?php echo $related_post_container; ?>>
+  </div>
 
 	<?php
 }
--- apl-templates/related-content/template.php
+++ apl-templates/related-content/template.php
@@ -13,6 +13,7 @@ $show_images = $layer['show_images'];
 $show_excerpts = $layer['show_excerpts'];
 $show_buttons = $layer['show_buttons'];
 $button_text = $layer['button_text'];
+$button_classes = ( isset( $layer['button_classes'] ) ) ? $layer['button_classes'] : null;
 $css_classes = ( isset( $layer['css_classes'] ) ) ? $layer['css_classes'] : null;
 $container = ( isset( $layer['container'] ) && !is_array( $layer['container'] ) ) ? $layer['container'] : 'container';
 $attributes = ( isset( $layer['attributes'] ) ) ? $layer['attributes'] : null;
