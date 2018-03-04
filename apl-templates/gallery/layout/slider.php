c9e8cda913914f2b7583364c7d2acaf5b36891ef
D.S. Webster <dswebs.me@gmail.com>
2018-03-04 16:01:04 -0500

    Add a count check to suppress slide navigation

--- apl-templates/gallery/layout/slider.php
+++ apl-templates/gallery/layout/slider.php
@@ -21,12 +21,18 @@
 	  <?php endforeach; ?>
 
   </div>
-  <a class="carousel-control-prev" href="#<?php echo $apl_unique_id; ?>-slider" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#<?php echo $apl_unique_id; ?>-slider" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+
+	<?php if( $count > 1 ): ?>
+
+		<a class="carousel-control-prev" href="#<?php echo $apl_unique_id; ?>-slider" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#<?php echo $apl_unique_id; ?>-slider" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+
+	<?php endif; ?>
+
 </div>
