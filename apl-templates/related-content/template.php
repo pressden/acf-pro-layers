3aad5143365189e17b5a7b2594b4ee72896cf80a
D.S. Webster <dswebs.me@gmail.com>
2018-07-13 00:34:24 -0400

    Add support for custom queries in the "Related Content" layer

--- apl-templates/related-content/template.php
+++ apl-templates/related-content/template.php
@@ -4,7 +4,7 @@ Template Name: APL Related Content
 */
 
 // layer fields
-$related_posts = $layer['posts'];
+$selection_method = $layer['selection_method'];
 $layout = ( isset( $layer['layout'] ) ) ? $layer['layout'] : 'grid';
 $columns = ( isset( $layer['columns'] ) && !is_array( $layer['columns'] ) ) ? $layer['columns'] : 3;
 $columns = ( $layout == 'slider' ) ? 1 : $columns; // force sliders to one column
@@ -17,22 +17,97 @@ $show_buttons = $layer['show_buttons'];
 $button_text = $layer['button_text'];
 $button_classes = ( isset( $layer['button_classes'] ) ) ? $layer['button_classes'] : null;
 $randomize = $layer['randomize'];
-$random_sets = $layer['random_sets'];
+$limit = $layer['limit'];
 $css_classes = ( isset( $layer['css_classes'] ) ) ? $layer['css_classes'] : null;
 $container = ( isset( $layer['container'] ) && !is_array( $layer['container'] ) ) ? $layer['container'] : 'container';
 $attributes = ( isset( $layer['attributes'] ) ) ? $layer['attributes'] : null;
+$count = 0; // count is required for some templates
 
-// count is required for some templates
-$count = 0;
+switch( $selection_method ) {
+	case 'manual':
+	$related_posts = $layer['posts'];
 
-// enable randomization
-if( $randomize ) {
-	shuffle( $related_posts );
+	// enable randomization
+	if( $randomize ) {
+		shuffle( $related_posts );
 
-	// display a specific number of sets ( $random_sets * $columns )
-	if( $random_sets ) {
-		$related_posts = array_slice( $related_posts, 0, ( $random_sets * $columns ) );
+		// limit restricts the output to a random sub-set on each page load
+		if( $limit ) {
+			$related_posts = array_slice( $related_posts, 0, $limit );
+		}
 	}
+
+	break;
+
+	case 'query':
+	$query_settings = $layer['query_settings'];
+	$post_type = ( isset( $query_settings['post_type'] ) ) ? $query_settings['post_type'] : array( 'post' );
+	$posts_per_page = ( isset( $query_settings['result_limit'] ) ) ? $query_settings['result_limit'] : 3;
+	$require_images = ( isset( $query_settings['result_limit'] ) ) ? $query_settings['result_limit'] : 0;
+	$taxonomies = ( isset( $query_settings['taxonomies'] ) ) ? $query_settings['taxonomies'] : null;
+	$preset_file = ( isset( $query_settings['preset'] ) ) ? plugin_dir_path( __FILE__ ) . 'queries/' . $query_settings['preset'] . '.php' : null;
+
+	// convert $post_type to an array
+	if( is_string( $post_type ) ) {
+		// split on comma and trim any whitespace
+		$post_type = preg_split( '/[\s*,\s*]*,+[\s*,\s*]*/', $post_type );
+	}
+
+	// set some default $args based on ACF values and common queries
+	$args['post_type'] = $post_type;
+	$args['posts_per_page'] = $posts_per_page;
+	$args['orderby'] = ( $randomize ) ? 'rand' : 'date';
+	$args['meta_query'] = array();
+	$args['tax_query'] = array();
+
+	// only include posts with a featured image
+	if( $show_images && $require_images ) {
+		$args['meta_query'][] = array(
+			'key' => '_thumbnail_id',
+			'compare' => 'EXISTS',
+		);
+	}
+
+	// add tax_query args
+	if( $taxonomies ) {
+		foreach( $taxonomies as $tax ) {
+			$taxonomy = $tax['taxonomy'];
+			$terms = $tax['terms'];
+			$field = $tax['field'];
+			$operator = $tax['operator'];
+
+			// convert $terms to an array
+			if( is_string( $terms ) ) {
+				// split on comma and trim any whitespace
+				$terms = preg_split( '/[\s*,\s*]*,+[\s*,\s*]*/', $terms );
+			}
+
+			// if the taxonomy exists we can query it
+			if( taxonomy_exists( $taxonomy ) ) {
+				$args['tax_query'][] = array(
+					'taxonomy' => $taxonomy,
+					'field' => $field,
+					'terms' => $terms,
+					'operator' => $operator,
+				);
+			}
+		}
+	}
+
+	// include a preset file which can override any of the above defaults
+	if( file_exists( $preset_file ) ) {
+		include( $preset_file );
+	}
+
+	// query the database with our custom $args
+	$query = new WP_Query( $args );
+
+	// if the query has posts
+	if( $query->have_posts() ) {
+		// output query results in an ACF friendly format
+		$related_posts = array_map( function( $post ) { return array( 'post' => $post ); }, $query->posts );
+	}
+	break;
 }
 
 apl_open_layer( $layer_name, $apl_unique_id, $css_classes, $attributes, $container );
