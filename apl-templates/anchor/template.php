2baf13e6bfdc6a9356f2d9c6d6322ef63d09f086
D.S. Webster <dswebs.me@gmail.com>
2018-07-25 13:51:47 -0400

    Add label to the anchor layer

--- apl-templates/anchor/template.php
+++ apl-templates/anchor/template.php
@@ -5,7 +5,9 @@ Template Name: APL Anchor
 
 // layer fields
 $name = $layer['name'];
+$label = ( isset( $layer['label'] ) ) ? $layer['label'] : null;
+$label_attribute = ( $label ) ? 'data-label="' . $label . '"' : '';
 $css_classes = ( isset( $layer['css_classes'] ) ) ? $layer['css_classes'] : null;
 ?>
 
-<a name="<?php echo $name; ?>" id="<?php echo $apl_unique_id; ?>" class="anchor <?php echo $css_classes; ?>"></a>
+<a name="<?php echo $name; ?>" id="<?php echo $apl_unique_id; ?>" class="anchor <?php echo $css_classes; ?>" <?php echo $label_attribute; ?>></a>
