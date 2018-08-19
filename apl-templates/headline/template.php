a617f47b95b9a45947353209bd16b616a28b56e5
D.S. Webster <dswebs.me@gmail.com>
2018-08-19 10:47:31 -0400

    Cleanup redundant tags

--- apl-templates/headline/template.php
+++ apl-templates/headline/template.php
@@ -11,9 +11,7 @@ $dek_tag = ( isset( $layer['dek_tag'] ) && !is_array( $layer['dek_tag'] ) ) ? $l
 $css_classes = ( isset( $layer['css_classes'] ) ) ? $layer['css_classes'] : null;
 $container = ( isset( $layer['container'] ) && !is_array( $layer['container'] ) ) ? $layer['container'] : 'container';
 $attributes = ( isset( $layer['attributes'] ) ) ? $layer['attributes'] : null;
-?>
 
-<?php
 if( $args['include_wrapper'] ) {
 	apl_open_layer( $layer_name, $apl_unique_id, $css_classes, $attributes, $container );
 }
