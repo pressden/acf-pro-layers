6a3f20195d16b1b18ba2470ce6f3ed01656fc68b
Marty OConnor <marty@boultoncreative.com>
2018-11-27 09:57:14 -0500

    code cleanup

--- apl-templates/hero/template.php
+++ apl-templates/hero/template.php
@@ -8,13 +8,11 @@ $title = $layer['title'];
 $excerpt = $layer['excerpt'];
 $button_text = $layer['button_text'];
 $button_link = $layer['button_link'];
-$background_image = $layer['background_image'];
+$background_image = $layer['poster_image'];
 $title_tag = ( isset( $layer['title_tag'] ) && !is_array( $layer['title_tag'] ) ) ? $layer['title_tag'] : 'h1';
 $background_video = $layer['background_video_grp'];
-$background_video_fallback_img = $background_video['video_fallback_image']['url'];
-$background_video_url = $background_video['video_url'];
-$background_video_type = $background_video['video_type'];
-
+$bg_video_url = $layer['video_url'];
+$bg_video_type = $layer['video_type'];
 $css_classes = ( isset( $layer['css_classes'] ) ) ? $layer['css_classes'] : null;
 $container = ( isset( $layer['container'] ) && !is_array( $layer['container'] ) ) ? $layer['container'] : 'container';
 $attributes = ( isset( $layer['attributes'] ) ) ? $layer['attributes'] : null;
@@ -27,40 +25,24 @@ if( $args['include_wrapper'] ) {
 ?>
 
 <?php
-    $video_fallback_image = '<div class="d-sm-none"><img src="' .$background_video_fallback_img . '"/></div>';
-    if ( $background_video_type == 'HTML5' ) {
-        $videoLayout = '<video class="d-none d-sm-block d-md-block" width="100%" autoplay muted><source src="' . $background_video_url . '" type="video/mp4"></video>' . $video_fallback_image;
-    } else if ( $background_video_type == 'YouTube' ){
-        $videoLayout = '<div class="d-none d-sm-block embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="' . $background_video_url . '" allowfullscreen></iframe></div>' . $video_fallback_image;
+    $video_fallback_image = '<div class="d-sm-none"><img src="' .$background_image['url'] . '"/></div>';
+    if ( $bg_video_type == 'HTML5' ) {
+        $videoLayout = '<video class="d-none d-sm-block d-md-block" width="100%" autoplay muted><source src="' . $bg_video_url . '" type="video/mp4"></video>' . $video_fallback_image;
+    } else if ( $bg_video_type == 'YouTube' ){
+        $videoLayout = '<div class="d-none d-sm-block embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="' . $bg_video_url . '" allowfullscreen></iframe></div>' . $video_fallback_image;
     }
 ?>
 
 <div class="hero-col px-0 <?php echo ( $args['include_column'] ) ? 'col' : ''; ?>">
     <div class="hero-image media-container card">
-
-        <?php/* 
-            // if ($video_url) {
-
-            } else if () {
-                // background image (hero image)
-            } else {
-                // color picker
-            }
-        
-        
-        
-        */?>
-
-     <?php
+        <?php 
             $background_image_tag = wp_get_attachment_image( $background_image['ID'], 'full-size', false, array( 'class' => 'img-fluid mx-auto d-block card-img', 'style' => 'width: 100%' ) );
-            if ( $background_image_tag && !$background_video_url ){
+            if ( $bg_video_url ){
+                echo $videoLayout;
+            } else if ( $background_image_tag ){
                 echo $background_image_tag;
-            } else if ( $background_video_url && $background_image_tag ){
-                echo $videoLayout;
-            } else if ( $background_video_url ) {
-                echo $videoLayout;
-            }
-        ?>     
+            } 
+        ?>      
         <?php if ( $title || $excerpt || $button_link ): ?>
             <div class="hero-info card-img-overlay d-flex justify-content-center align-items-center flex-column">
                 <div class="hero-text-inner-container">
