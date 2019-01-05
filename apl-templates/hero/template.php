87d82a7975fffa569fd6479d0a438be097181fe0
D.S. Webster <dswebs.me@gmail.com>
2019-01-04 19:14:48 -0500

    Refactor the hero layer to improve logic and fix markup structure

--- apl-templates/hero/template.php
+++ apl-templates/hero/template.php
@@ -26,7 +26,7 @@ $video_fallback_image = '<div class="d-sm-none"><img src="' .$background_image['
 
 switch( $bg_video_type ) {
   case 'HTML5':
-  $videoLayout = '<video class="d-none d-sm-block d-md-block" width="100%" autoplay muted><source src="' . $bg_video_url . '" type="video/mp4"></video>' . $video_fallback_image;
+  $videoLayout = '<video class="d-none d-sm-block" width="100%" autoplay muted><source src="' . $bg_video_url . '" type="video/mp4"></video>' . $video_fallback_image;
   break;
 
   case 'YouTube':
@@ -40,43 +40,46 @@ switch( $bg_video_type ) {
 ?>
 
 <div class="hero-col px-0 <?php echo ( $args['include_column'] ) ? 'col' : ''; ?>">
-  <div class="hero-image media-container card">
+
+  <div class="hero-media <?php echo ( $bg_video_url ) ? 'hero-video' : 'hero-image'; ?> media-container">
+
     <?php
     // Determine video vs. poster UI.
-    $background_image_tag = wp_get_attachment_image( $background_image['ID'], 'full-size', false, array( 'class' => 'img-fluid mx-auto d-block card-img', 'style' => 'width: 100%' ) );
+    $background_image_tag = wp_get_attachment_image( $background_image['ID'], 'full-size', false, array( 'class' => 'img-fluid' ) );
     if ( $bg_video_url ) {
       echo $videoLayout;
     } else if ( $background_image_tag ) {
       echo $background_image_tag;
     }
-
-    // Make sure one of the 3 types of desired data exists to render content div
-    // Check again inside block to determine final UI render
-    if ( $title || $excerpt || $button_link ): ?>
-
-      <div class="hero-info card-img-overlay d-flex justify-content-center align-items-center flex-column">
-        <div class="hero-text-inner-container">
-
-          <?php if( $title ) { echo '<' . $title_tag . ' class="hero-headline" ' . '>' . $title . '</' . $title_tag . '>'; } ?>
-
-          <?php if ( $excerpt ): ?>
-            <div class="hero-excerpt">
-              <p><?php echo $excerpt; ?></p>
-            </div>
-          <?php endif; ?>
-
-          <?php if ( $button_link ): ?>
-            <div class="hero-button btn btn-primary">
-              <a href="<?php echo $button_link['url'];?>" class=""><?php echo $button_text; ?></a>
-            </div>
-          <?php endif; ?>
-
-        </div>
-      </div>
-
-    <?php endif; ?>
+		?>
 
   </div>
+
+	<?php
+	// Make sure one of the 3 types of desired data exists to render content div
+  // Check again inside block to determine final UI render
+  if ( $title || $excerpt || $button_link ): ?>
+
+    <div class="hero-info">
+
+      <?php if( $title ) { echo '<' . $title_tag . ' class="hero-title" ' . '>' . $title . '</' . $title_tag . '>'; } ?>
+
+      <?php if ( $excerpt ): ?>
+        <div class="hero-excerpt">
+          <p><?php echo $excerpt; ?></p>
+        </div>
+      <?php endif; ?>
+
+      <?php if ( $button_link ): ?>
+        <div class="hero-button btn btn-primary">
+          <a href="<?php echo $button_link['url'];?>" class=""><?php echo $button_text; ?></a>
+        </div>
+      <?php endif; ?>
+
+    </div>
+
+  <?php endif; ?>
+
 </div>
 
 <?php
